81dc079c6c30068f48f6a9f374e511ecdcf5fa6f
dv-Termux <g.h.com>
2021-06-23 19:03:20 +0100

    added 2shambavi with a working AND SIMPLE menu

---
+++ 2Shambavi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+
+Smenu () {
+echo -e "Shambavi Bash App\n"
+echo "Menu"
+
+echo -e "(S)tart \n(V)iew logs \n(Q)uit"
+echo -e "\nEnter Option > "; read _cursor
+
+if [ $_cursor == q ]; then echo "stop"; exit; fi
+if [ $_cursor == s ]; then echo "bla"; fi
+}
+Smenu
+
