efa53a4ba3048800bddc43b6f375ee90ced4e168
dv-fedoraOS <flowreshe.seiva.d.arve@gmail.com>
2021-06-26 19:06:41 +0100

    2Shambavi.sh changes, but it's a mess

--- 2Shambavi.sh
+++ 2Shambavi.sh
@@ -4,13 +4,40 @@
 # AND add an alias to change them
 source ./all/textFormating/mainColorScheme
 
+clrH_echoMenu () {
+	clear
+}
+_start () {
+	clear
+	echo -e "\nThis is the menu Start"
+       	echo "wait"
+	sleep 1
+	#firstMenu
+
+}
+
+# Menu for the View option
+_view () {
+	clear
+	echo -e "\nThis is the menu View"
+       	echo "wait"
+	sleep 1
+	#firstMenu
+
+}
+
+
+
 
 # Showing first menu and asking what to do
-firstMenu () {
+_firstMenu () {
 clear
 echo -e "${_red}Shambavi Bash App${_end}\n"
 echo "Menu"
-echo -e "${_underG}S${_end}tart (V)iew logs (Q)uit"
+}
+_firstMenu
+_secondMenu () {
+echo -e "(${_underG}S${_end}tart) (${_underG}V${_end}iew logs) (${_underG}Q${_end}uit)"
 
 # Prompt which option And start Redirecting
 echo -en "\nEnter Option > "; read _cursor
@@ -19,7 +46,15 @@ if [ $_cursor == s ];
 	then 
 		echo "You choose S, Start"; 
 		sleep 2
+		_start
+fi
+
+if [ $_cursor == v ]; 
+	then 
+		echo "You choose V, View"; 
+		sleep 2
+		_view
 fi
 }
-firstMenu
+_firstMenu
 
