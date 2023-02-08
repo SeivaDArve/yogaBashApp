cb4625fc33f17fa0513e58921f0b6741b82163ae
seivadarve <flowreshe.seiva.d.arve@gmail.com>
2023-02-08 12:57:39 +0000

    Improvement shambavi.sh: better SELECT menu

--- shambavi.sh
+++ shambavi.sh
@@ -8,33 +8,32 @@ clear
 
 
 
-echo "Welcomo to Shambavi Maha Mudra"
+echo "Welcome to Shambavi Maha Mudra"
 echo ""
 echo "choose your option, ok?"
 
 PS1='Please enter yourr choice: '
 options=("Option 1" "Option 2" "Option 3" "Quit")
 select opt in "${options[@]}"
-
 do
-	case $opt in
-		"Option 1")
-			echo "you chose choice 1"
-			;;
+   case $opt in
+      "Option 1")
+         echo "you chose choice 1"
+         ;;
 
-		"Option 2")
-				echo "you chose choice 2"
-				;;
+      "Option 2")
+         echo "you chose choice 2"
+         ;;
 
-			"Option 3")
-				echo "you chose choice 2"
-				;;
+      "Option 3")
+         echo "you chose choice 2"
+         ;;
 
-			"Quit")
-				break
-				;;
+      "Quit")
+         break
+         ;;
 
-			*) 	echo "invalid option" $REPLY;;
-		esac
-	done
-	
+      *) 	echo "invalid option" $REPLY;;
+   esac
+done
+
