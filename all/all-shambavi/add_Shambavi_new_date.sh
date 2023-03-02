825bccb1d3aee1c2018178a4870ce6f4ea543096
seivadarve <flowreshe.seiva.d.arve@gmail.com>
2023-03-02 18:49:59 +0000

    Improvement of script: add_Shambavi_new_date.sh

--- all/all-shambavi/add_Shambavi_new_date.sh
+++ all/all-shambavi/add_Shambavi_new_date.sh
@@ -1,6 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+clear
+echo "Simply appending a new date at the end of thr file"
+echo " > ./shamb-dates.txt"
 echo
+echo "press ANY KEY or wait 5 secs"
+read -s -n 1 -t 5
 date >> ./shamb-dates.txt
+echo
 echo "New date added to \"shamb-dates.txt\" "
 echo
+echo "Do you want to perform a '$ git commit?'"
+echo " > uDev: Not ready yet"
