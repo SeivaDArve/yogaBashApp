7f46982af2dee227ad5ac35afb99eb47276f3d2a
seivadarve <flowreshe.seiva.d.arve@gmail.com>
2023-03-15 12:31:44 +0000

    Improved + Bug fix: add_Shambavi_new_date.sh

--- all/all-shambavi/add_Shambavi_new_date.sh
+++ all/all-shambavi/add_Shambavi_new_date.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 clear
-echo "Simply appending a new date at the end of thr file"
+echo "Simply appending a new date at the end of the file:"
 echo " > ./shamb-dates.txt"
 echo
 echo "press ANY KEY or wait 5 secs"
@@ -9,6 +9,9 @@ read -s -n 1 -t 5
 date >> ./shamb-dates.txt
 echo
 echo "New date added to \"shamb-dates.txt\" "
+echo
+echo " > Last 5 lines of file \"./shamb-dates.txt\" are:"
+tail -n 5 ./shamb-dates.txt
 echo
 echo "Do you want to perform a '$ git commit?'"
 echo " > uDev: Not ready yet"
